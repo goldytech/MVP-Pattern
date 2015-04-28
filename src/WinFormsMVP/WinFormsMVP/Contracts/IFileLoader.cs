9f99584b61471e363e91a0ecfa4ebfb809b1c24f
Muhammad Afzal <afzal.qureshi@outlook.com>
2015-04-28 12:13:37 +0530

    Added the contracts of FileLoader and ProductMapper

---
+++ src/WinFormsMVP/WinFormsMVP/Contracts/IFileLoader.cs
@@ -0,0 +1,17 @@
+﻿namespace WinFormsMVP.Contracts
+{
+    using System.IO;
+
+    /// <summary>
+    ///    Represents the contract File Loader Service
+    /// </summary>
+    public interface IFileLoader
+    {
+        /// <summary>
+        /// Loads the specified file name.
+        /// </summary>
+        /// <param name="fileName">Name of the file.</param>
+        /// <returns>Stream of opened file</returns>
+        Stream Load(string fileName); 
+    }
+}
---
+++ src/WinFormsMVP/WinFormsMVP/Contracts/IProductMapper.cs
@@ -0,0 +1,19 @@
+﻿namespace WinFormsMVP.Contracts
+{
+    using System.Xml;
+
+    using WinFormsMVP.Models;
+
+    /// <summary>
+    /// Represents Product Mapper Service
+    /// </summary>
+    public interface IProductMapper
+    {
+        /// <summary>
+        /// Maps the Product POCO specified XML Elements.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        /// <returns></returns>
+        Product Map(XmlReader reader); 
+    }
+}
