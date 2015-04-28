7c19a425c8f4bad5f004b8e9f59daeadd5c22d70
Muhammad Afzal <afzal.qureshi@outlook.com>
2015-04-28 11:43:50 +0530

    Added product POCO

---
+++ src/WinFormsMVP/WinFormsMVP/Models/Product.cs
@@ -0,0 +1,38 @@
+﻿namespace WinFormsMVP.Models
+{
+    /// <summary>
+    /// Represents Product Entity
+    /// </summary>
+    public class Product
+    {
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        /// <value>The id.</value>
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the product name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the product unit price.
+        /// </summary>
+        /// <value>The unit price.</value>
+        public decimal UnitPrice
+        {
+            get;
+            set;
+        }
+    }
+}
